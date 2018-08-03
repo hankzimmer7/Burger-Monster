b601ec7298f3c1c9a416e330217f0ed99ded07ef
hankzimmer7 <hankzimmer7@gmail.com>
2018-08-02 21:42:38 -0400

    changes footer

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,8 @@
 INSERT INTO burgers (burger_name) VALUES ('Hamburger');
 INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
 INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Bacon Ranger Burger');
+INSERT INTO burgers (burger_name) VALUES ('Southwest Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Barbecue Burger');
+INSERT INTO burgers (burger_name) VALUES ('Guacamole Burger');
